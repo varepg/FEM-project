b824123bff1c34dec371fc8ac1893508dbf5115e
Elias <elias.bjork@icloud.com>
2023-05-11 13:29:41 +0200

    add solver for transient temperature distribution

--- solve.py
+++ solve.py
@@ -1,7 +1,8 @@
 import calfem.core as cfc
 import numpy as np
 from gripper import GripperGeometry, GripperMesh
-from utils import get_eq
+from utils import get_C, get_eq
+from scipy import linalg
 from typing import Tuple
 from numpy.typing import NDArray
 
@@ -43,3 +44,28 @@ def stat_temp_dist(
     a, r = cfc.solveq(K, f, bc)
 
     return mesh, a, r, K, f
+
+
+def transient_temp_dist(gripper: GripperGeometry, T_inf: float, h: float, dt: float):
+    mesh, a_stat, _, K, f = stat_temp_dist(gripper, T_inf, h)
+    ndofs = np.size(mesh.dofs)
+
+    C = get_C(gripper, mesh)
+
+    idx_max = np.argmax(a_stat)
+    pt9a_stat_max = 0.9*a_stat[idx_max]
+
+    a = T_inf * np.ones((ndofs, 1))
+    a_new = a
+
+    A = C+dt*K
+    dtf = dt*f
+
+    while a_new[idx_max] < pt9a_stat_max:
+        b = (C@a[:,-1]).reshape(ndofs, 1) + dtf
+        a_new = linalg.solve(A, b)
+        a = np.hstack((a, a_new))
+
+    nbr_steps = np.shape(a)[1]
+
+    return a, nbr_steps
